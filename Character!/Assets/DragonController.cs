36aac2d5cdf88687e61deb38d0e9a0ad36267879
Unknown <arslanfatihfurkan@gmail.com>
2017-02-19 00:59:01 +0300

    eklendi.

---
+++ Character!/Assets/DragonController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragonControllerScript : MonoBehaviour
+{
+
+    public float maxSpeed = 10f;
+    bool facingLeft = true;
+    public Vector2 velocity;
+    private Rigidbody2D rigi;
+
+    void Awake()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rigi.velocity = new Vector2(1f, 2f);
+        if (move > 0 && !facingLeft)
+            Flip();
+        if (move < 0 && facingLeft)
+            Flip();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 theScale = transform.localScale;
+        theScale *= -1;
+        transform.localScale = theScale;
+    }
+
+}
